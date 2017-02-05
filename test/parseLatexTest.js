8cfe43b4724119e2decdacf28ffd15f15b801876
lasalvavida <rht25@drexel.edu>
2017-02-05 18:44:23 -0500

    Push additions and tests

--- test/parseLatexTest.js
+++ test/parseLatexTest.js
@@ -1,91 +1,133 @@
-import assert from 'assert'
+import {expect} from 'chai'
 import parseLatex from '../lib/parseLatex'
 
 describe('parseLatex', function () {
-  it('can parse a simple latex document', function () {
-    var latex = '' +
-      '\\documentclass[12pt]{article}\n' +
-      '\\usepackage{amsmath}\n' +
-      '\\title{\\LaTeX}\n' +
-      '\\date{}\n' +
-      '\\begin{document}\n' +
-      '  \\maketitle\n' +
-      '  \\LaTeX{} is a document preparation system for\n' +
-      '  the \\TeX{} typesetting program. It offers\n' +
-      '  programmable desktop publishing features and\n' +
-      '  extensive facilities for automating most\n' +
-      '  aspects of typesetting and desktop publishing,\n' +
-      '  including numbering and  cross-referencing,\n' +
-      '  tables and figures, page layout,\n' +
-      '  bibliographies, and much more. \\LaTeX{} was\n' +
-      '  originally written in 1984 by Leslie Lamport\n' +
-      '  and has become the  dominant method for using\n' +
-      '  \\TeX; few people write in plain \\TeX{} anymore.\n' +
-      '  The current version is \\LaTeXe.\n' +
-      '  \n' +
-      '  % This is a comment, not shown in final output.\n' +
-      '  % The following shows typesetting  power of LaTeX:\n' +
-      '  \\begin{align}\n' +
-      '    E_0 &= mc^2                              \\\\\n' +
-      '    E &= \\frac{mc^2}{\\sqrt{1-\\frac{v^2}{c^2}}}\n' +
-      '  \\end{align}\n' +
-      '\\end{document}\n'
-    var environment = parseLatex(latex)
-    expect(environment).toEqual({
-      name: 'documentclass',
-      type: 'command',
-      arguments: [
-        {
-          value: '12pt',
-          type: 'optional'
-        },
-        {
-          value: 'article',
-          type: 'required'
-        }
-      ],
-    }, {
-      name: 'usepackage',
-      type: 'command',
-      arguments: [
-        {
-          value: 'amsmath',
-          type: 'required'
-        }
-      ]
-    }, {
-      name: 'title',
-      type: 'command',
-      arguments: [
-        {
-          value: {
-            name: 'LaTeX'.
+  it('can parse a latex command', function () {
+    var environment = parseLatex('\\documentclass[12pt]{article}')
+    expect(environment).to.deep.equal([
+      {
+        name: 'documentclass',
+        type: 'command',
+        arguments: [
+          {
+            value: '12pt',
+            optional: true
+          }, {
+            value: 'article',
+            optional: false
+          }
+        ]
+      }
+    ])
+  })
+
+  it('can parse two latex commands', function () {
+    var environment = parseLatex('\\documentclass[12pt]{article}\n' +
+      '\\usepackage{amsmath}')
+    expect(environment).to.deep.equal([
+      {
+        name: 'documentclass',
+        type: 'command',
+        arguments: [
+          {
+            value: '12pt',
+            optional: true
+          }, {
+            value: 'article',
+            optional: false
+          }
+        ]
+      }, {
+        name: 'usepackage',
+        type: 'command',
+        arguments: [
+          {
+            value: 'amsmath',
+            optional: false
+          }
+        ]
+      }
+    ])
+  })
+
+  it('can parse latex commands with text', function () {
+    var environment = parseLatex('\\maketitle\n' +
+      '\\LaTeX{} was originally written in 1984 by Leslie Lamport and has become the dominant method for using \\TeX; ' +
+      'few people write in plain \\TeX{} anymore.')
+    expect(environment).to.deep.equal([
+      {
+        name: 'maketitle',
+        type: 'command',
+        arguments: []
+      }, {
+        name: 'LaTeX',
+        type: 'command',
+        arguments: [
+          {
+            value: '',
+            optional: false
+          }
+        ]
+      },
+      ' was originally written in 1984 by Leslie Lamport and has become the dominant method for using ', {
+        name: 'TeX',
+        type: 'command',
+        arguments: []
+      },
+      '; few people write in plain ', {
+        name: 'TeX',
+        type: 'command',
+        arguments: [
+          {
+            value: '',
+            optional: false
+          }
+        ]
+      },
+      ' anymore.'
+    ])
+  })
+
+  it('can parse nested commands', function () {
+    var environment = parseLatex('\\title{\\LaTeX}')
+    expect(environment).to.deep.equal([
+      {
+        name: 'title',
+        type: 'command',
+        arguments: [
+          {
+            value: [
+              {
+                name: 'LaTeX',
+                type: 'command',
+                arguments: []
+              }
+            ],
+            optional: false
+          }
+        ]
+      }
+    ])
+  })
+
+  it('can parse environments', function () {
+    var environment = parseLatex('\\begin{document}\n' +
+        '    The current version is \\LaTeXe.' +
+        '\\end{document}'
+    )
+    expect(environment).to.deep.equal([
+      {
+        name: 'document',
+        type: 'environment',
+        arguments: [],
+        children: [
+          'The current version is ', {
+            name: 'LaTeXe',
             type: 'command',
             arguments: []
-          },
-          type: 'required'
-        }
-      ]
-    }, {
-      name: 'date',
-      type: 'command',
-      arguments: []
-    }, {
-      name: 'document',
-      type: 'environment',
-      arguments: [],
-      children: [
-        {
-          name: 'maketitle',
-          type: 'command',
-          arguments: []
-        }, {
-          name: 'LaTeX',
-          type: 'command',
-          arguments: []
-        },
-        ' is a document preparation system for\n'
-      ]
-    })
+          }
+        ]
+      }
+    ])
   })
 })
